fdecc64fce05705bbf15fa2cf61cf93384e11edf
ashmit1795 <ashmitpatra77@gmail.com>
2025-08-17 12:29:44 +0530

    Refactor server startup logic and add Vercel configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { app } from "./app.js";
-import { PORT } from "./config/env.js";
+import { NODE_ENV, PORT } from "./config/env.js";
 
-app.listen(PORT, () => {
-	console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+
+if (NODE_ENV === "development") {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
 
 // Export the app for Vercel
 export default app;
